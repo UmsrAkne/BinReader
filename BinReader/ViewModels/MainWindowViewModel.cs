2e17b00b598f948d836f929d03dba2a756be4e59
Akn <sibata486@gmail.com>
2023-08-26 16:00:42 +0900

    add /

--- BinReader/ViewModels/MainWindowViewModel.cs
+++ BinReader/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,18 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.Generic;
+using BinReader.Models;
+using Prism.Mvvm;
 
 namespace BinReader.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        public string Title => "Binary Reader"; 
+        public string Title => "Binary Reader";
+
+        public List<BinaryPiece> BinaryPieces { get; private set; } = new List<BinaryPiece>();
+
+        public void SetBinary(byte[] bytes)
+        {
+            BinaryPieces.Add(new BinaryPiece(bytes));
+        }
     }
 }
