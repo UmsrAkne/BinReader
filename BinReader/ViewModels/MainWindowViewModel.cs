880f580e1a7790a11d1d032bcf0cd2e47a1af214
Akn <sibata486@gmail.com>
2023-08-29 23:50:16 +0900

    add feat / MainWindowViewModel に BinaryPiece のリストをセットするメソッドを実装

--- BinReader/ViewModels/MainWindowViewModel.cs
+++ BinReader/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,43 @@
-﻿using Prism.Mvvm;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using BinReader.Models;
+using Prism.Mvvm;
 
 namespace BinReader.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        public string Title => "Binary Reader"; 
+        public string Title => "Binary Reader";
+
+        public string SearchPattern { get; set; } = string.Empty;
+        
+        public ObservableCollection<BinaryPiece> BinaryPieces { get; private set; }
+
+        /// <summary>
+        /// this.SearchPattern に入力されているテキストを元に、入力値から BinaryPieces のリストを生成。
+        /// BinaryPieces にセットします。
+        /// this.SearchPattern に不正な値が入力されている場合は処理中断します。
+        /// </summary>
+        /// <param name="bytes">BinaryPieces の元となる byte の配列</param>
+        public void SetBinaries(byte[] bytes)
+        {
+            if (!HexConverter.CanConvert(SearchPattern))
+            {
+                return;
+            }
+            
+            var checker = new BinaryChecker();
+            checker.SearchPattern = HexConverter.ToByteArray(SearchPattern);
+            foreach (var b in bytes)
+            {
+                checker.IsMatched(b);
+            }
+            
+            var matches = checker.MatchedHeaderAddress;
+
+            var splitter = new BinarySplitter();
+            BinaryPieces = new ObservableCollection<BinaryPiece>(
+                splitter.SplitArray(bytes, matches).Select(b => new BinaryPiece(b)));
+        }
     }
 }
--- BinReaderTest/ViewModels/MainWindowViewModelTest.cs
+++ BinReaderTest/ViewModels/MainWindowViewModelTest.cs
@@ -1,3 +1,4 @@
+using BinReader.ViewModels;
 using NUnit.Framework;
 
 namespace TestProject2.ViewModels
@@ -5,5 +6,17 @@ namespace TestProject2.ViewModels
     [TestFixture]
     public class MainWindowViewModelTest
     {
+        [Test]
+        public void SetBinariesTest()
+        {
+            var vm = new MainWindowViewModel
+            {
+                SearchPattern = "01 02",
+            };
+
+            vm.SetBinaries(new byte[] { 0x0, 0x1, 0x2, 0x3, });
+            CollectionAssert.AreEqual(vm.BinaryPieces[0].RawBytes, new byte[] { 0x0, });
+            CollectionAssert.AreEqual(vm.BinaryPieces[1].RawBytes, new byte[] { 0x1, 0x2, 0x3, });
+        }
     }
 }
