eda7b288daf67b6cf521d040243b8a052405142d
Akn <sibata486@gmail.com>
2023-08-30 00:18:10 +0900

    add feat / BinaryPiece のバイナリを Shift-Jis の文字列で取得するプロパティを実装

--- BinReader/Models/BinaryPiece.cs
+++ BinReader/Models/BinaryPiece.cs
@@ -1,8 +1,12 @@
+using System.Text;
+
 namespace BinReader.Models
 {
     public class BinaryPiece
     {
         public byte[] RawBytes { get; }
+
+        public string Text => System.Text.Encoding.GetEncoding(932).GetString(RawBytes);
 
         public BinaryPiece(byte[] bytes)
         {
