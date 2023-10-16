f8721947196772b9630563811424b74b2da7c356
Akn <sibata486@gmail.com>
2023-10-16 15:02:07 +0900

    add test / BinarySplitter.Split のテストを追加

--- BinReaderTest/Models/BinarySplitterTest.cs
+++ BinReaderTest/Models/BinarySplitterTest.cs
@@ -12,7 +12,7 @@ namespace TestProject2.Models
         public void SplitArrayTest()
         {
             var splitter = new BinarySplitter();
-            var target = new byte[] { 0, 1, 2, 3, 4,};
+            var target = new byte[] { 0, 1, 2, 3, 4, };
             var splitAddresses = new List<int> { 1, 3, };
 
             var results = splitter.SplitArray(target, splitAddresses).ToList();
@@ -28,8 +28,8 @@ namespace TestProject2.Models
         public void SplitArrayTest_2()
         {
             var splitter = new BinarySplitter();
-            var target = new byte[] { 0, 1, 2, 3, 4,};
-            var splitAddresses = new List<int> { 1, 3, 4,};
+            var target = new byte[] { 0, 1, 2, 3, 4, };
+            var splitAddresses = new List<int> { 1, 3, 4, };
 
             var results = splitter.SplitArray(target, splitAddresses).ToList();
 
@@ -50,6 +50,40 @@ namespace TestProject2.Models
 
             CollectionAssert.AreEqual(result[0].RawBytes, new byte[] { 0, 1, 2, });
             CollectionAssert.AreEqual(result[1].RawBytes, new byte[] { 3, 4, });
+        }
+
+        [Test]
+        public void SplitTest_複数回分割()
+        {
+            var target = new byte[] { 0, 1, 2, 3, 1, 2, };
+            var pattern = new List<byte>() { 1, 2, };
+            var result = BinarySplitter.Split(target, pattern);
+
+            CollectionAssert.AreEqual(result[0].RawBytes, new byte[] { 0, 1, 2, });
+            CollectionAssert.AreEqual(result[1].RawBytes, new byte[] { 3, 1, 2, });
+        }
+
+        [Test]
+        public void SplitTest_パターン一桁()
+        {
+            var target = new byte[] { 0, 1, 2, 0, 1, 2, };
+            var pattern = new List<byte>() { 0, };
+            var result = BinarySplitter.Split(target, pattern);
+
+            CollectionAssert.AreEqual(result[0].RawBytes, new byte[] { 0, });
+            CollectionAssert.AreEqual(result[1].RawBytes, new byte[] { 1, 2, 0, });
+            CollectionAssert.AreEqual(result[2].RawBytes, new byte[] { 1, 2, });
+        }
+
+        [Test]
+        public void SplitTest_パターン無し()
+        {
+            var target = new byte[] { 0, 1, 2, 0, 1, 2, };
+
+            var patternZero = new List<byte>();
+            var patternNull = new List<byte>();
+            CollectionAssert.AreEqual(target, BinarySplitter.Split(target, patternZero).First().RawBytes);
+            CollectionAssert.AreEqual(target, BinarySplitter.Split(target, patternNull).First().RawBytes);
         }
     }
 }
