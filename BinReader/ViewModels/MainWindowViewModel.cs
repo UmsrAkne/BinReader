ea37926b09efc602bec3db119be992ab248f1fd8
Akn <sibata486@gmail.com>
2023-10-17 20:01:41 +0900

    del feat / MainWindowViewModel から不要となった機能を削除した

--- BinReader/ViewModels/MainWindowViewModel.cs
+++ BinReader/ViewModels/MainWindowViewModel.cs
@@ -15,38 +15,6 @@ namespace BinReader.ViewModels
 
         public ObservableCollection<BinaryPiece> BinaryPieces { get; private set; }
 
-        /// <summary>
-        /// this.SearchPattern に入力されているテキストを元に、入力値から BinaryPieces のリストを生成。
-        /// BinaryPieces にセットします。
-        /// this.SearchPattern に不正な値が入力されている場合は処理中断します。
-        /// </summary>
-        /// <param name="bytes">BinaryPieces の元となる byte の配列</param>
-        public void SetBinaries(byte[] bytes)
-        {
-            if (!HexConverter.CanConvert(SearchPattern))
-            {
-                return;
-            }
-            
-            var checker = new BinaryChecker
-            {
-                SearchPattern = HexConverter.ToByteArray(SearchPattern),
-            };
-
-            foreach (var b in bytes)
-            {
-                checker.IsMatched(b);
-            }
-            
-            var matches = checker.MatchedHeaderAddress;
-
-            var splitter = new BinarySplitter();
-            BinaryPieces = new ObservableCollection<BinaryPiece>(
-                splitter.SplitArray(bytes, matches).Select(b => new BinaryPiece(b)));
-            
-            RaisePropertyChanged(nameof(BinaryPieces));
-        }
-
         public void LoadBinary(byte[] bytes)
         {
             var pattern = HexConverter.ToByteArray(SearchPattern).ToList();
