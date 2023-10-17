290608af629179da6ff25079295451cffb5cb09d
Akn <sibata486@gmail.com>
2023-10-17 19:45:30 +0900

    add prop / BinaryPiece に切り分けたアドレスの情報をつけた

--- BinReader/Models/BinaryPiece.cs
+++ BinReader/Models/BinaryPiece.cs
@@ -4,6 +4,8 @@ namespace BinReader.Models
     {
         public byte[] RawBytes { get; }
 
+        public long Address { get; set; }
+
         public string Text => System.Text.Encoding.GetEncoding(932).GetString(RawBytes);
 
         public BinaryPiece(byte[] bytes)
