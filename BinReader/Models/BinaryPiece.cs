623b7f69caea9560832edc6b4c098ca26627422d
Akn <sibata486@gmail.com>
2023-10-20 23:37:07 +0900

    add feat / 生のバイト配列から指定した長さのヘッダーとフッターを取得可能にした

--- BinReader/Models/BinaryPiece.cs
+++ BinReader/Models/BinaryPiece.cs
@@ -4,6 +4,9 @@ namespace BinReader.Models
 {
     public class BinaryPiece
     {
+        private readonly int headerLength;
+        private readonly int footerLength;
+
         public byte[] RawBytes { get; }
 
         public string ByteArrayString
@@ -19,9 +22,22 @@ namespace BinReader.Models
 
         public string Text => System.Text.Encoding.GetEncoding(932).GetString(RawBytes);
 
+        public byte[] Header =>
+            RawBytes.Length <= headerLength ? RawBytes : RawBytes.Take(headerLength).ToArray();
+
+        public byte[] Footer =>
+            RawBytes.Length <= footerLength ? RawBytes : RawBytes.Skip(RawBytes.Length - footerLength).ToArray();
+
         public BinaryPiece(byte[] bytes)
         {
             RawBytes = bytes;
+        }
+
+        public BinaryPiece(byte[] bytes, int headerLength, int footerLength)
+        {
+            RawBytes = bytes;
+            this.headerLength = headerLength;
+            this.footerLength = footerLength;
         }
     }
 }
--- BinReaderTest/Models/BinaryPieceTest.cs
+++ BinReaderTest/Models/BinaryPieceTest.cs
@@ -12,5 +12,29 @@ namespace TestProject2.Models
             var bp = new BinaryPiece(new byte[] { 0x61, 0x62, });
             Assert.That("ab", Is.EqualTo(bp.Text), "0x61, 0x62 は Shift-Jis でそれぞれ a, b を表す");
         }
+
+        [Test]
+        [TestCase(new byte[] { }, 0, new byte[] { })]
+        [TestCase(new byte[] { }, 1, new byte[] { })]
+        [TestCase(new byte[] { 0x0, 0x1, 0x2, }, 0, new byte[] { })]
+        [TestCase(new byte[] { 0x0, 0x1, 0x2, }, 1, new byte[] { 0x0, })]
+        [TestCase(new byte[] { 0x0, 0x1, 0x2, }, 2, new byte[] { 0x0, 0x1, })]
+        public void HeaderTest(byte[] bytes, int length, byte[] except)
+        {
+            var bp = new BinaryPiece(bytes, length, 0);
+            CollectionAssert.AreEqual(except, bp.Header);
+        }
+
+        [Test]
+        [TestCase(new byte[] { }, 0, new byte[] { })]
+        [TestCase(new byte[] { }, 1, new byte[] { })]
+        [TestCase(new byte[] { 0x0, 0x1, 0x2, }, 0, new byte[] { })]
+        [TestCase(new byte[] { 0x0, 0x1, 0x2, }, 1, new byte[] { 0x2, })]
+        [TestCase(new byte[] { 0x0, 0x1, 0x2, }, 2, new byte[] { 0x1, 0x2, })]
+        public void FooterTest(byte[] bytes, int length, byte[] except)
+        {
+            var bp = new BinaryPiece(bytes, 0, length);
+            CollectionAssert.AreEqual(except, bp.Footer);
+        }
     }
 }
